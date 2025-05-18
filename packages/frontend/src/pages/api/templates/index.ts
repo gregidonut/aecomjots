c0f39bb9b9faab781f3b727ddeaab81a3ef7c372
Gregory Osilaja <gregosilaja@outlook.com>
2025-05-18 00:17:01 +0000

    ref: use axios for the frontend api as well
    starting to think fetch might actually be better for this but we'll see

--- packages/frontend/src/pages/api/templates/index.ts
+++ packages/frontend/src/pages/api/templates/index.ts
@@ -1,19 +1,18 @@
 import type { APIRoute } from "astro";
+import axios from "axios";
 export const GET: APIRoute = async function ({ locals }) {
     const { getToken } = locals.auth();
     const token = await getToken();
     const ASTRO_API_URL = import.meta.env.ASTRO_API_URL as string;
-    const resp = await fetch(`${ASTRO_API_URL}/templates`, {
+    const resp = await axios({
         method: "GET",
+        url: `${ASTRO_API_URL}/templates`,
         headers: {
             Authorization: `Bearer ${token}`,
         },
     });
-
-    const data = await resp.json();
-
-    return new Response(JSON.stringify(data), {
-        status: 200,
+    return new Response(JSON.stringify(resp.data), {
+        status: resp.status,
         headers: { "Content-Type": "application/json" },
     });
 };
